15d56573b47f7827a5e36deaae7a5af0c4e5651a
Clever Bytes <kcy493@gmail.com>
2020-05-12 16:00:28 +0500

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule EASY.MixProject do
   defp deps do
     [{:csv, "~> 2.3.1"},
     {:ecto_sql, "~> 3.0", override: true},
-      {:ecto, "~> 3.3.3"},
+      {:ecto, "~> 3.4.4"},
       {:acl, ">= 0.0.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
